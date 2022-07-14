f0c2522c55dd7d2c6f83e98011a2749eee9bf1ea
Roberto Gonzalez <roberto.gonzalez@unosquare.com>
2022-07-14 14:15:33 -0500

    proyecto unosquare

---
+++ nightwatch.conf.js
@@ -0,0 +1,29 @@
+module.exports = {
+    
+    custom_commands_path: "./commands",
+    page_objects_path:[ 'PageObjects/Unosquare','PageObjects/Nightwatch'],
+   
+
+    src_folders: ["tests"],
+    webdriver: {
+        start_process: true,
+        port: 4444,
+        server_path: require('chromedriver').path,
+        cli_args: [
+        ]
+    },
+    test_settings: {
+        default: {
+            launch_url: 'https://nightwatchjs.org',
+            desiredCapabilities: {
+                browserName: 'chrome',
+                loggingPrefs: { 'browser': 'ALL' },
+                chromeOptions : {
+                    "args" : ["start-maximized"]
+                }
+            }
+        }
+    }
+
+   
+};
